6f931990cc812e7e7742fd3bf59f99c249453926
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-11-05 16:47:30 +0800

    change structure of goroutine

--- routine.go
+++ routine.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"log"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"github.com/liserjrqlxue/acmg2015"
 	"github.com/liserjrqlxue/goUtil/simpleUtil"
 	"github.com/liserjrqlxue/goUtil/textUtil"
 )
@@ -91,8 +93,52 @@ func getAvd(fileName string, dbChan chan<- []map[string]string, throttle, writeE
 	<-throttle
 }
 
-func writeAvd(excel *excelize.File, dbChan chan []map[string]string, num int, throttle chan bool) {
+func WriteAvd(excel *excelize.File, runDmd, runAvd chan bool) {
 	log.Println("Write AVD Start")
+	var avdArray []string
+	if *avdFiles != "" {
+		avdArray = strings.Split(*avdFiles, ",")
+	}
+	if *avdList != "" {
+		avdArray = append(avdArray, textUtil.File2Array(*avdList)...)
+	}
+	if len(avdArray) > 0 {
+		log.Println("Start load AVD")
+		// acmg
+		acmg2015.AutoPVS1 = *autoPVS1
+		var acmgCfg = simpleUtil.HandleError(textUtil.File2Map(*acmgDb, "\t", false)).(map[string]string)
+		for k, v := range acmgCfg {
+			acmgCfg[k] = filepath.Join(dbPath, v)
+		}
+		acmg2015.Init(acmgCfg)
+
+		// wait runDmd done
+		runDmd <- true
+		var (
+			runWrite   = make(chan bool, 1)
+			throttle   = make(chan bool, *threshold)
+			writeExcel = make(chan bool, *threshold)
+			size       = len(avdArray)
+			dbChan     = make(chan []map[string]string, size)
+		)
+
+		// goroutine writeAvd in case block getAvd since more avd than threshold
+		runWrite <- true
+		go writeAvd(excel, dbChan, size, runWrite)
+		for _, fileName := range avdArray {
+			throttle <- true
+			go getAvd(fileName, dbChan, throttle, writeExcel)
+		}
+		// wait writeAvd done
+		runWrite <- true
+	} else {
+		log.Println("Write AVD Skip")
+	}
+	log.Println("Write AVD Done")
+	<-runAvd
+}
+
+func writeAvd(excel *excelize.File, dbChan chan []map[string]string, size int, throttle chan bool) {
 	var sheetName = *avdSheetName
 	var rows = simpleUtil.HandleError(excel.GetRows(sheetName)).([][]string)
 	var title = rows[0]
@@ -105,16 +151,31 @@ func writeAvd(excel *excelize.File, dbChan chan []map[string]string, num int, th
 			writeRow(excel, sheetName, item, title, rIdx)
 		}
 		count++
-		if count == num {
+		if count == size {
 			close(dbChan)
 		}
 	}
-	log.Println("Write AVD Done")
 	<-throttle
 }
 
-func writeDmd(excel *excelize.File, dmdArray []string, throttle chan bool) {
+func WriteDmd(excel *excelize.File, dmdFiles, dmdList string, throttle chan bool) {
 	log.Println("Write DMD Start")
+	var dmdArray []string
+	if dmdFiles != "" {
+		dmdArray = strings.Split(dmdFiles, ",")
+	}
+	if dmdList != "" {
+		dmdArray = append(dmdArray, textUtil.File2Array(dmdList)...)
+	}
+	if len(dmdArray) > 0 {
+		writeDmd(excel, dmdArray)
+	} else {
+		log.Println("Write DMD Skip")
+	}
+	log.Println("Write DMD Done")
+}
+
+func writeDmd(excel *excelize.File, dmdArray []string) {
 	var sheetName = *dmdSheetName
 	var rows = simpleUtil.HandleError(excel.GetRows(sheetName)).([][]string)
 	var title = rows[0]
@@ -124,15 +185,65 @@ func writeDmd(excel *excelize.File, dmdArray []string, throttle chan bool) {
 		for _, item := range dmd {
 			rIdx++
 			updateDmd(item)
+			var sampleID = item["#Sample"]
+			var gene = item["gene"]
+			var CopyNum, err = strconv.Atoi(item["CopyNum"])
+			simpleUtil.CheckErr(err, "can not convert CopyNum of "+item["CopyNum"])
+			if CopyNum != 2 {
+				var geneInfo, ok = SampleGeneInfo[sampleID]
+				if !ok {
+					geneInfo = make(map[string]*GeneInfo)
+					geneInfo[gene] = &GeneInfo{
+						基因:   gene,
+						cnv:  true,
+						cnv0: CopyNum == 0,
+					}
+					SampleGeneInfo[sampleID] = geneInfo
+				} else {
+					var info, ok = geneInfo[gene]
+					if !ok {
+						geneInfo[gene] = &GeneInfo{
+							基因:   gene,
+							cnv:  true,
+							cnv0: CopyNum == 0,
+						}
+					} else {
+						info.cnv = true
+						info.cnv0 = info.cnv0 || CopyNum == 0
+					}
+				}
+			}
 			updateINDEX(item, rIdx)
 			writeRow(excel, sheetName, item, title, rIdx)
 		}
 	}
-	log.Println("Write DMD Done")
+}
+
+func WriteAe(excel *excelize.File, dipinResult, smaResult string, throttle chan bool) {
+	log.Println("Write AE Start")
+	var db = make(map[string]map[string]string)
+	if dipinResult != "" {
+		var dipin, _ = textUtil.File2MapArray(dipinResult, "\t", nil)
+		for _, item := range dipin {
+			updateDipin(item, db)
+		}
+	}
+	if smaResult != "" {
+		var sma, _ = textUtil.File2MapArray(smaResult, "\t", nil)
+		for _, item := range sma {
+			updateSma(item, db)
+		}
+	}
+	if len(db) > 0 {
+		writeAe(excel, db)
+	} else {
+		log.Println("Write AE Skip")
+	}
+	log.Println("Write AE Done")
 	<-throttle
 }
 
-func writeAe(excel *excelize.File, db map[string]map[string]string, throttle chan bool) {
+func writeAe(excel *excelize.File, db map[string]map[string]string) {
 	log.Println("Write AE Start")
 	var rows = simpleUtil.HandleError(excel.GetRows(*aeSheetName)).([][]string)
 	var title = rows[0]
@@ -144,7 +255,6 @@ func writeAe(excel *excelize.File, db map[string]map[string]string, throttle cha
 		writeRow(excel, *aeSheetName, item, title, rIdx)
 	}
 	log.Println("Write AE Done")
-	<-throttle
 }
 
 func updateINDEX(item map[string]string, rIdx int) {
