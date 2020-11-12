e3c4bf6772f11a8933fc66502708c0b0b9665baf
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-11-12 16:58:32 +0800

    AutoFilter for batchCnv

--- routine.go
+++ routine.go
@@ -283,6 +283,8 @@ func writeBatchCnv(throttle chan bool) {
 		addDiseases2Cnv(item, batchCnvDiseaseTitle, batcnCnvSep, genes...)
 		writeRow(bcExcel, sheetName, item, BatchCnvTitle, i+2)
 	}
+	var lastCellName = simpleUtil.HandleError(excelize.CoordinatesToCellName(len(BatchCnvTitle), len(BatchCnv)+1)).(string)
+	simpleUtil.CheckErr(bcExcel.AutoFilter(sheetName, "A1", lastCellName, "TableStyleLight2"))
 	simpleUtil.CheckErr(bcExcel.SaveAs(*prefix + ".batchCNV.xlsx"))
 	<-throttle
 }
