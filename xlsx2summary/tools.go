1c29e19e5641c6ee7410ebcc235ec13230788f0d
Wang Yaoshen <wangyaoshen@bgi.com>
2021-04-15 12:34:53 +0800

    :art:split main and build func

--- xlsx2summary/tools.go
+++ xlsx2summary/tools.go
@@ -230,3 +230,79 @@ func cHgvsAlt(cHgvs string) string {
 	}
 	return cHgvs
 }
+
+func loadDB() map[string]Info {
+	var db = make(map[string]Info)
+	// 读解读表
+	for n, annoFile := range strings.Split(*anno, ",") {
+		log.Printf("信息：开始读取解读表%d[%s]\n", n+1, annoFile)
+		var annoExcel = simpleUtil.HandleError(excelize.OpenFile(annoFile)).(*excelize.File)
+
+		getInfoFromAVD(db, annoExcel)
+		getInfoFromCNV(db, annoExcel)
+		getInfoFromAE(db, annoExcel)
+	}
+	return db
+}
+
+func fillExcel(strSlice [][]string, db map[string]Info, outExcel *excelize.File, sheetName string) {
+	for rIdx, strArray := range strSlice {
+		if rIdx < titleRowIndex-3 {
+			continue
+		} else if rIdx == titleRowIndex-3 {
+			checkTitleName(strArray, "基因一", rIdx+1, geneNameCIdx)
+			if len(strArray) > colLength && strArray[colLength] != "" {
+				log.Fatalf("错误：表头(%d,%d)[%s]!=\"\"", rIdx+1, colLength+1, strArray[colLength])
+			}
+			continue
+		} else if rIdx == titleRowIndex-2 {
+			checkTitleName(strArray, resultTitle, rIdx+1, resultCIdx)
+			checkTitleName(strArray, "基因名称", rIdx+1, geneNameCIdx)
+			checkTitleName(strArray, "变异一", rIdx+1, geneNameCIdx+1)
+			checkTitleName(strArray, "疾病", rIdx+1, geneNameCIdx+mutLit*mutColCount+1)
+			checkTitleName(strArray, "患病风险", rIdx+1, geneNameCIdx+mutLit*mutColCount+2)
+			checkTitleName(strArray, "遗传方式", rIdx+1, geneNameCIdx+mutLit*mutColCount+3)
+			continue
+		} else if rIdx == titleRowIndex-1 {
+			checkTitleName(strArray, sampleIDTitle, rIdx+1, sampleIDCIdx)
+			checkTitleName(strArray, summaryTitle, rIdx+1, summaryCIdx)
+			checkTitleName(strArray, "疾病一", rIdx+1, resultCIdx)
+			checkTitleName(strArray, "外显子", rIdx+1, geneNameCIdx+1)
+			checkTitleName(strArray, "碱基改变", rIdx+1, geneNameCIdx+2)
+			checkTitleName(strArray, "氨基酸改变", rIdx+1, geneNameCIdx+3)
+			continue
+		}
+		rIdx++
+		var index = rIdx - titleRowIndex
+		if len(strArray) < sampleIDCIdx || strArray[sampleIDCIdx-1] == "" {
+			log.Printf("信息：样品%3d为空，跳过\n", index)
+			continue
+		}
+		var sampleID = strArray[sampleIDCIdx-1]
+		if len(strArray) > colLength && strArray[colLength] != "" {
+			log.Printf("警告：样品%3d[%11s]已有时间戳，跳过\n", index, sampleID)
+			continue
+		}
+		var item, ok = db[sampleID]
+		if !ok {
+			if sampleCount[sampleID] == 0 {
+				log.Printf("警告：样品%3d[%11s]无解读信息，跳过\n", index, sampleID)
+				continue
+			} else {
+				log.Printf("信息：样品%3d[%11s]无疾病\n", index, sampleID)
+				item = Info{
+					sampleID: sampleID,
+					基因检测结果总结: "无疾病",
+				}
+			}
+		}
+		if sampleCount[sampleID] > 1 {
+			log.Printf("警告：样品%3d[%11s]解读信息重复%d次\n", index, sampleID, sampleCount[sampleID])
+		}
+
+		item.sortGene()
+		item.resumeGene()
+
+		item.fillExcel(outExcel, sampleID, sheetName, index, rIdx)
+	}
+}
