48512fc48865a54e601f56c5cc01a9a39a156786
Wang Yaoshen <wangyaoshen@bgi.com>
2021-04-15 11:27:24 +0800

    :art:move struct to struct.go and add method

--- xlsx2summary/main.go
+++ xlsx2summary/main.go
@@ -15,36 +15,6 @@ import (
 	"github.com/liserjrqlxue/version"
 )
 
-type Info struct {
-	sampleID   string
-	可疑, 携带     int
-	基因检测结果     []string
-	基因         []GeneInfo
-	geneList   []string
-	geneMap    map[string]GeneInfo
-	样本快递日期     string
-	新筛编号       string
-	华大基因检测编号   string
-	性别         string
-	生化筛查结果     string
-	生化筛查结果拟诊   string
-	基因检测结果报告日期 string
-	基因检测结果总结   string
-}
-
-type GeneInfo struct {
-	基因名称 string
-	变异   []MutInfo
-	疾病   string
-	患病风险 string
-	遗传方式 string
-}
-type MutInfo struct {
-	外显子   string
-	碱基改变  string
-	氨基酸改变 string
-}
-
 // os
 var (
 	ex, _   = os.Executable()
@@ -80,7 +50,7 @@ var 患病风险 map[string]string
 
 var sampleCount = make(map[string]int)
 
-func main() {
+func init() {
 	version.LogVersion()
 	flag.Parse()
 	if *anno == "" || *input == "" {
@@ -91,7 +61,9 @@ func main() {
 	if *prefix == "" {
 		*prefix = *input
 	}
+}
 
+func main() {
 	患病风险 = simpleUtil.HandleError(textUtil.File2Map(filepath.Join(etcPath, "患病风险.txt"), "\t", false)).(map[string]string)
 
 	var outExcel, err = excelize.OpenFile(*input)
@@ -204,42 +176,9 @@ func main() {
 		if sampleCount[sampleID] > 1 {
 			log.Printf("警告：样品%3d[%11s]解读信息重复%d次\n", index, sampleID, sampleCount[sampleID])
 		}
-		// 按患病风险排序基因
-		var head, tail []string
-		for _, geneSymbol := range item.geneList {
-			var geneInfo = item.geneMap[geneSymbol]
-			if geneInfo.患病风险 == "可能患病" {
-				head = append(head, geneSymbol)
-			} else {
-				tail = append(tail, geneSymbol)
-			}
-		}
-		item.geneList = append(head, tail...)
 
-		for _, geneSymbol := range item.geneList {
-			var geneInfo = item.geneMap[geneSymbol]
-			item.基因 = append(item.基因, geneInfo)
-			switch geneInfo.患病风险 {
-			case "可能患病":
-				item.可疑++
-				item.基因检测结果 = append(item.基因检测结果, geneInfo.疾病+"/可疑")
-			case "携带者":
-				item.携带++
-				item.基因检测结果 = append(item.基因检测结果, geneInfo.疾病+"/携带")
-			}
-			if item.可疑 > 0 && item.携带 > 0 {
-				item.基因检测结果总结 = fmt.Sprintf("%d个疾病可疑+%d个疾病携带", item.可疑, item.携带)
-			} else if item.可疑 > 0 {
-				item.基因检测结果总结 = fmt.Sprintf("%d个疾病可疑", item.可疑)
-			} else if item.携带 > 0 {
-				item.基因检测结果总结 = fmt.Sprintf("%d个疾病携带", item.携带)
-			} else {
-				item.基因检测结果总结 = "无疾病"
-			}
-		}
-		if len(item.geneList) == 0 {
-			item.基因检测结果总结 = "无疾病"
-		}
+		item.sortGene()
+		item.resumeGene()
 
 		WriteCellStr(outExcel, sheetName, summaryCIdx, rIdx, item.基因检测结果总结)
 		// 填充 基因检测结果
